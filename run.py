7d2fe0bddfb2efd946f0e1b6b282ad7ae58cb0be
jcabre04 <cabrera.student@gmail.com>
2022-02-11 17:22:47 -0800

    Populated database with examples. Added flask shell (pre-loaded imports)

--- run.py
+++ run.py
@@ -1,4 +1,9 @@
-from app import app
+from app import app, db
+from app.models import User, Session, Skill
+
+@app.shell_context_processor
+def make_shell_processor():
+    return {'db':db, 'User': User, 'Session': Session, 'Skill': Skill}
 
 if __name__ == "__main__":
     app.run()
