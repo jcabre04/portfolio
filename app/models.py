81dd718d9a6fd0e0d5501100547cf617a04210cd
jcabre04 <cabrera.student@gmail.com>
2022-02-12 14:42:20 -0800

    Added login functionality

--- app/models.py
+++ app/models.py
@@ -1,7 +1,16 @@
 from datetime import datetime
-from app import db
 
-class User(db.Model):
+from app import db, login
+
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask_login import UserMixin
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True, nullable=False)
     email = db.Column(db.String(120), index=True, unique=True, nullable=False)
@@ -16,6 +25,12 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 bridge_session_skill = db.Table('bridge_session_skill',
     db.Column('session_id', db.Integer, db.ForeignKey('session.id'),
