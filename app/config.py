2ef3642ff9b13c71f36bf91ce5566f2697c7cd41
jcabre04 <cabrera.student@gmail.com>
2022-04-10 22:05:46 -0700

    Added default environment variables to db connector. Added default user

--- app/config.py
+++ app/config.py
@@ -4,8 +4,20 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
+    if "RDS_HOSTNAME" in os.environ:
+        user = os.environ["RDS_USERNAME"]
+        passw = os.environ["RDS_PASSWORD"]
+        host = os.environ["RDS_HOSTNAME"]
+        port = os.environ["RDS_PORT"]
+        db_name = os.environ["RDS_DB_NAME"]
+        SQLALCHEMY_DATABASE_URI = (
+            f"mysql+mysqldb://{user}:{passw}@{host}:{port}/{db_name}"
+        )
+    else:
+        SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(
+            basedir, "app.db"
+        )
+
     SECRET_KEY = os.environ.get("SECRET_KEY") or "test_secret_key"
-    SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL"
-    ) or "sqlite:///" + os.path.join(basedir, "app.db")
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
